066204ae26d364a5946d5a126944b520755b0703
Chungsang Tom Lam <erinata@gmail.com>
2023-11-14 11:53:47 -0500

    loop through many parameters

--- run_knn.py
+++ run_knn.py
@@ -15,7 +15,22 @@ data = pandas.get_dummies(data, columns=['Sex'])
 data = data.values
 # print(data)
 
-machine = KNeighborsRegressor(n_neighbors=10, weights='distance')
-return_values = kfold_template.run_kfold(machine, data, target, 4, True)
-average_return_value = sum(return_values)/len(return_values)
-print(average_return_value)
+trials = []
+for w in ['uniform', 'distance']:
+  for k in range(1,50):
+    machine = KNeighborsRegressor(n_neighbors=k, weights='uniform')
+    return_values = kfold_template.run_kfold(machine, data, target, 4, True)
+    average_return_value = sum(return_values)/len(return_values)
+    # print(average_return_value)
+    trials.append((average_return_value, k, w))
+    
+trials.sort(key=lambda x: x[0], reverse=True)
+
+print(trials[:5])
+
+
+
+
+
+
+
